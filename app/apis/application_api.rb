036891927d113851cb95e9870903ec56322f01f8
Flori Garcia <flori.garciavicente@gmail.com>
2015-03-31 17:59:05 -0500

    created user routes

--- app/apis/application_api.rb
+++ app/apis/application_api.rb
@@ -2,7 +2,8 @@ class ApplicationApi < Grape::API
   format :json
   extend Napa::GrapeExtenders
 
-  mount HelloApi => '/'
+  # mount HelloApi => '/'
+  mount UsersApi => '/'
 
   add_swagger_documentation
 end
